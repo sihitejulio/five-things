01d8ddccaca2dba2cad01c88c7ee081a8539de04
julio p sihite <julio.sihite@finaccel.co>
2022-09-10 06:46:58 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
+app.use('/dist', express.static(__dirname + 'dist'))
+app.use('/js', express.static(__dirname + 'js'))
+app.use('/img', express.static(__dirname + 'img'))
+app.use('/css', express.static(__dirname + 'css'))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
